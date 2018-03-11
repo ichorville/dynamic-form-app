33411cc075b71dbe4058e17f16401ffa264cfe91
Jay <mjzeel@gmail.com>
2018-03-11 11:19:07 +0530

    Initial commit - Initial test cases

---
+++ test/index.test.js
@@ -0,0 +1,38 @@
+import chai from "chai";
+
+require('jsdom-global')()
+
+// Import the idf class instance
+import idf from "../src/App.es6";
+
+chai.should();
+
+// create mock html tag
+document.body.innerHTML = "<div>Sample text in div</div>";
+
+describe('#IDF Library Test', function () {
+
+	describe('#Element Selector', function () {
+		it('should be an object', function () {
+			ghost('div').selector.should.be.an('object');
+		});
+	});
+
+	describe('#DivContent', function () {
+		it('should be a string', function () {
+			ghost('div').html().should.be.a('string');
+		});
+
+		it('should equal Sample text in div', function () {
+			ghost('div').html().should.equal('Sample text in div');
+		});
+
+		it('should equal paragraph text', function () {
+			ghost('div').html('<p>changed value</p>').should.equal('<p>changed value</p>');
+		});
+
+		it('should equal empty string', function () {
+			ghost('div').html('').should.equal('');
+		});
+	});
+});
